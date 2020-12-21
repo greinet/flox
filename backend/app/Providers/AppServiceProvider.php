0fcb6692d05e7690a04663a0e49dcaabf75d53e7
Andreas Greiner <agreiner@vmd51557.contaboserver.net>
2020-12-21 16:12:19 +0100

    Added force https

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      $this->app['request']->server->set('HTTPS', true);
+      URL::forceScheme('https');
     }
-
     /**
      * Register any application services.
      *
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+           $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
